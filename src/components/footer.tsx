014cb2066e55db63883962b408db47ed07767875
steven.yeh <notiger.yeh@gmail.com>
2023-04-12 02:41:32 +0800

    feat:update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent
-} from '@radix-ui/react-popover';
 import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 interface SocialIconProps {
@@ -11,66 +6,27 @@ interface SocialIconProps {
   Icon: React.FC;
 }
 
-interface SocialIconWithPopoverProps extends SocialIconProps {
-  label: string;
-}
-
 export default function Footer() {
   return (
-    <footer className="bg-gray-100 py-8 flex flex-col">
-      <div className="container mx-auto px-4">
-        <div className="flex flex-col items-center space-y-4 md:space-y-0 md:flex-row md:justify-between">
-          <div className="flex space-x-4">
-            <SocialIconWithPopover
-              link="https://twitter.com"
-              Icon={FaTwitter}
-              label="Follow me on Twitter"
-            />
-            <SocialIconWithPopover
-              link="https://github.com"
-              Icon={FaGithub}
-              label="Check out my GitHub"
-            />
-            <SocialIconWithPopover
-              link="https://linkedin.com"
-              Icon={FaLinkedin}
-              label="Connect with me on LinkedIn"
-            />
-          </div>
+    <footer className="bg-gray-100 py-8">
+      <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
+        <div className="flex space-x-4">
+          <SocialIcon link="https://twitter.com" Icon={FaTwitter} />
+          <SocialIcon link="https://github.com" Icon={FaGithub} />
+          <SocialIcon link="https://linkedin.com" Icon={FaLinkedin} />
+        </div>
+        <div className="text-sm text-gray-600">
+          &copy; {new Date().getFullYear()} Your Name. All rights reserved.
         </div>
       </div>
-      <div className="mt-auto text-center text-sm text-gray-600 py-4">
-        &copy; {new Date().getFullYear()} Your Name. All rights reserved.
-      </div>
     </footer>
-  );
-}
-
-function SocialIconWithPopover({
-  link,
-  Icon,
-  label
-}: SocialIconWithPopoverProps) {
-  return (
-    <Popover>
-      <PopoverTrigger>
-        <SocialIcon link={link} Icon={Icon} />
-      </PopoverTrigger>
-      <PopoverContent
-        side="top"
-        align="center"
-        className="rounded-md shadow-lg bg-white p-2"
-      >
-        {label}
-      </PopoverContent>
-    </Popover>
   );
 }
 
 function SocialIcon({ link, Icon }: SocialIconProps) {
   return (
     <a href={link} target="_blank" rel="noreferrer">
-      <Icon className="text-xl" />
+      <Icon />
     </a>
   );
 }
