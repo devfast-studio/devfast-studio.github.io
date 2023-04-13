c8d8451606474344d008ebe5735b727917239d7b
steven.yeh <notiger.yeh@gmail.com>
2023-04-14 00:32:07 +0800

    fix: correct spelling, add select none for her section

--- src/pages/home/hero/index.tsx
+++ src/pages/home/hero/index.tsx
@@ -5,14 +5,14 @@ import AnimatedButton from './animatedButton';
 
 export default function Hero() {
   return (
-    <section id="hero" className="relative w-screen h-[50vh]">
+    <section id="hero" className="relative w-screen h-[50vh] select-none">
       <ParticleBackground />
       <div className="absolute inset-0 flex flex-col justify-center items-center">
         <AnimatedText
           className="text-[20px] md:text-[33px] text-center font-bold text-gray-800 mx-[5vw] md:mx-[10vw] leading-12"
           phrases={[
             'First, solve the problem. Then, write the code.\n - John Johnson',
-            'Do one thing and do it well.\n - Doug McIlroy',
+            'Do one thing and do it well.\n - Douglas McIlroy',
             'Simplicity is prerequisite for reliability.\n - Edsger W. Dijkstra',
             'Simplicity is the soul of efficiency.\n - Austin Freeman'
           ]}
